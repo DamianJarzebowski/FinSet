3fc452aca8797d362630e0b6207d7d92d5a8f6da
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-02-11 19:32:17 +0100

    Damian commit

---
+++ src/main/java/DJ/FinSet/FinSetApplication.java
@@ -0,0 +1,13 @@
+package DJ.FinSet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DJ/FinSet/FinSetApplicationTests.java
@@ -0,0 +1,13 @@
+package DJ.FinSet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
