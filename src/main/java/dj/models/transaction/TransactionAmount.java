4f3cd8d8345c3746b9ccdac4c4618e47021b586c
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-02-12 03:07:47 +0100

    add transaction model

---
+++ src/main/java/dj/models/transaction/TransactionAmount.java
@@ -0,0 +1,16 @@
+package dj.models.transaction;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Accessors(chain = true)
+public class TransactionAmount {
+
+    private String amount;
+
+    private String currency;
+
+}
